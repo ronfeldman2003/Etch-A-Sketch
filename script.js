e0f5e1bf7b5fd3d1e8872109f38102f75d4aa11f
ronfeldman <ronfeldman2003@gmail.com>
2023-05-01 17:06:35 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const sketch_container = document.querySelector('div.sketch-container');
 const new_grid_button = document.getElementById("new-grid");
 new_grid_button.addEventListener('click',new_grid);
-function new_grid(e){
+build_sketch(16);
+function new_grid(){
     let grid_size = prompt("Please enter your grid length (betwwen 1-100)");
-    console.log(grid_size);
     if(grid_size==null){return;}else 
     if(grid_size>100||grid_size<1||(grid_size/grid_size)!=1){
        new_grid(); 
@@ -19,8 +19,7 @@ function build_sketch(size){
         pixel.classList.add('sketch-pixel');
         sketch_container.appendChild(pixel);
         const basis = (1/size)*100;
-        console.log(basis)
-        pixel.style.flexBasis=`${basis}%`;
+        pixel.style.flexBasis=`${basis}%`;  
         
     }
     start_listener();
@@ -29,7 +28,6 @@ function start_listener(){
     const pixels = document.querySelectorAll('div.sketch-pixel');
     pixels.forEach(pixel=>pixel.addEventListener('mouseover',color_in));
 }
-function color_in(e){
-    console.log(this);
+function color_in(){
     this.style.backgroundColor = "black";
 }
